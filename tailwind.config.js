ebc86e41639d36e76c49a3d124a5c7a84838ba22
Muhammed Sinan P <iam.muhammedsinan.p@gmail.com>
2024-07-06 18:31:57 +0530

    Create Bmi sample

---
+++ tailwind.config.js
@@ -0,0 +1,17 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ["./public/*.html"],
+  theme: {
+    extend: {
+
+    },
+  },
+  variants: {
+    extend: {
+      backgroundColor: ["active"],
+
+    },
+  },
+  plugins: [],
+}
+
